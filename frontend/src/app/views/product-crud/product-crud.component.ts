89588287a9084c672a95a88303d26594e6ce19b1
airtonfilho04 <airtonfilhocontato@outlook.com>
2020-07-03 00:23:24 -0300

    Finishing project

--- frontend/src/app/views/product-crud/product-crud.component.ts
+++ frontend/src/app/views/product-crud/product-crud.component.ts
@@ -1,3 +1,4 @@
+import { HeaderService } from './../../components/template/header/header.service';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router'
 
@@ -8,8 +9,12 @@ import { Router } from '@angular/router'
 })
 export class ProductCrudComponent implements OnInit {
 
-  constructor(private router: Router) { 
-    
+  constructor(private router: Router, private headerService: HeaderService) {
+    headerService.headerData = {
+      title: 'Cadastro de Produtos',
+      icon: 'storefront',
+      routeUrl: '/products'
+    }
   }
 
   ngOnInit(): void {
@@ -18,4 +23,5 @@ export class ProductCrudComponent implements OnInit {
   navigateToProductCreate(): void {
     this.router.navigate(['/products/create'])
   }
+
 }
